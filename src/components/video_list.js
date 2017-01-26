a73419fc70826ed02a1ffb915809aaa7b9a9a654
munens <kaumbuthom@gmail.com>
2017-01-26 13:00:58 -0800

    Sec. 2, lec. 26: Video list items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import VideoListItem from 'react'
 
 // In a functional component like below we pass props in through the arguments.
 // In a class based component, the props are passed in through the constructor.
 const VideoList = (props) => {
 
+    // create an array of video list items:
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video} />
+    })
+
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {videoItems}
         </ul>
     )   
 }
